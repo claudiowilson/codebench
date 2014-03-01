48d23ee8b51b961198dd6d49992a4e9675ca8433
Kevin Yeh <kevinyeah@utexas.edu>
2014-03-01 00:18:29 -0500

    Upvotes

--- queries.js
+++ queries.js
@@ -2,75 +2,75 @@ var pg = require('pg');
 var settings = require('./settings');
 
 var GetSubmissionsForQuestion = function(questionId, callback) {
-	CallPreparedStatement({name: 'get_submissions_for_question', text: "SELECT * FROM codebench.submission WHERE question=$1", values: [questionId]}, callback);
+    CallPreparedStatement({name: 'get_submissions_for_question', text: "SELECT * FROM codebench.submission WHERE question=$1", values: [questionId]}, callback);
 }
 
 var GetSubmissionsForUser = function(userId, callback) {
-	CallPreparedStatement({name: 'get_submissions_for_user', text: "SELECT * FROM codebench.submission WHERE submitted_user=$1", values: [userId]}, callback);
+    CallPreparedStatement({name: 'get_submissions_for_user', text: "SELECT * FROM codebench.submission WHERE submitted_user=$1", values: [userId]}, callback);
 }
 
 var GetQuestionsForUser = function(userId, callback) {
-	CallPreparedStatement({name: 'get_questions_for_user', text: "SELECT * FROM codebench.question WHERE asked_user=$1", values: [userId]}, callback);
+    CallPreparedStatement({name: 'get_questions_for_user', text: "SELECT * FROM codebench.question WHERE asked_user=$1", values: [userId]}, callback);
 }
 
 var GetQuestion = function(id, callback) {
-	CallPreparedStatement( {name: 'get_user', text: "SELECT * FROM codebench.question WHERE question_id=$1", values : [id] }, function(err, result) {
-		if(err) {
-			callback(err);
-		} else {
-			if(result.rows[0] == 'undefined') {
-				callback(new Error('No such question exists!'));
-			} else {
-				callback(null, result);
-			}
-		}
-	})
+    CallPreparedStatement( {name: 'get_user', text: "SELECT * FROM codebench.question WHERE question_id=$1", values : [id] }, function(err, result) {
+        if(err) {
+            callback(err);
+        } else {
+            if(result.rows[0] == 'undefined') {
+                callback(new Error('No such question exists!'));
+            } else {
+                callback(null, result);
+            }
+        }
+    })
 }
 
 var GetQuestionAndSubmissions = function(questionId, callback) {
-	CallPreparedStatement( { name: 'get_questions_and_submissions', text: "SELECT * FROM codebench.submission INNER JOIN codebench.user ON codebench.submission.submitted_user = codebench.user.user_id WHERE codebench.submission.question=$1", values : [questionId]}, callback);
+    CallPreparedStatement( { name: 'get_questions_and_submissions', text: "SELECT * FROM codebench.submission INNER JOIN codebench.user ON codebench.submission.submitted_user = codebench.user.user_id WHERE codebench.submission.question=$1", values : [questionId]}, callback);
 }
 
 var GetQuestions = function(callback) {
-	CallPreparedStatement("SELECT * FROM codebench.question LIMIT 50", callback);
+    CallPreparedStatement("SELECT * FROM codebench.question INNER JOIN codebench.user ON codebench.question.asked_user = codebench.user.user_id LIMIT 50", callback);
 }
 
 var GetQuestionVote = function(userId, questionId, callback) {
-    CallPreparedState("SELECT 1 FROM codebench.qvote WHERE codebench.qvote.user_id=$1 AND codebench.qvote.question_id=$2", callback);
+    CallPreparedState({ name: 'get_question_vote', text:"SELECT 1 FROM codebench.qvote WHERE codebench.qvote.user_id=$1 AND codebench.qvote.question_id=$2", values: [userId, questionId]}, callback);
 }
 
 var GetSubmissionVote = function(userId, submissionId, callback) {
-    CallPreparedState("SELECT 1 FROM codebench.svote WHERE codebench.svote.user_id=$1 AND codebench.svote.submission_id=$2", callback);
+    CallPreparedState({ name: 'get_submission_vote', text:"SELECT 1 FROM codebench.svote WHERE codebench.svote.user_id=$1 AND codebench.svote.submission_id=$2", values: [userId, submmissionId]}, callback);
 }
 
 var LoginUser = function(username, password, callback) {
-	CallPreparedStatement( {name: 'login_user', text: "SELECT username, password = crypt($1, password), user_id, full_name FROM codebench.user WHERE username=$2", values : [password, username] }, function(err, result) {
-		if(err) {
-			callback(err);
-		} else {
-			if(result.rows.length == 0 || result.rows[0] == 'undefined') {
-				callback(new Error('No such user exists!'));
-			} else {
-				callback(null, result);
-			}
-		}
-	})
+    CallPreparedStatement( {name: 'login_user', text: "SELECT username, password = crypt($1, password), user_id, full_name FROM codebench.user WHERE username=$2", values : [password, username] }, function(err, result) {
+        if(err) {
+            callback(err);
+        } else {
+            if(result.rows.length == 0 || result.rows[0] == 'undefined') {
+                callback(new Error('No such user exists!'));
+            } else {
+                callback(null, result);
+            }
+        }
+    })
 }
 
 var AddSubmission = function(submittedUserId, question, message, callback) {
-	CallPreparedStatement( { name: 'add_sumission', text : "INSERT INTO codebench.submission (submitted_user, question, message) VALUES($1, $2, $3) RETURNING submission_id", values: [submittedUserId, question, message] }, callback);
+    CallPreparedStatement( { name: 'add_sumission', text : "INSERT INTO codebench.submission (submitted_user, question, message) VALUES($1, $2, $3) RETURNING submission_id", values: [submittedUserId, question, message] }, callback);
 }
 
 var AddCodeForSubmission = function(submissionId, code, className, callback) {
-	CallPreparedStatement( { name: 'add_code_for_submission', text : "INSERT INTO codebench.code (submission_id, code, class_name) VALUES($1, $2, $3) RETURNING code_id", values: [submissionId, code, className] }, callback);	
+    CallPreparedStatement( { name: 'add_code_for_submission', text : "INSERT INTO codebench.code (submission_id, code, class_name) VALUES($1, $2, $3) RETURNING code_id", values: [submissionId, code, className] }, callback);
 }
 
 var AddQuestion = function(askedUserId, problem, input, output, callback) {
-	CallPreparedStatement( { name: 'add_question', text : "INSERT INTO codebench.question (asked_user, problem, input, output) VALUES ($1,$2,$3,$4) RETURNING question_id", values: [askedUserId, problem, input, output] }, callback);
+    CallPreparedStatement( { name: 'add_question', text : "INSERT INTO codebench.question (asked_user, problem, input, output) VALUES ($1,$2,$3,$4) RETURNING question_id", values: [askedUserId, problem, input, output] }, callback);
 }
 
 var AddUserPrepared = function(name, username, password, email, callback) {
-	CallPreparedStatement( { name: 'add_user', text: "INSERT INTO codebench.user (username, password, full_name, email) VALUES ($1, crypt($2, gen_salt('bf')), $3, $4) RETURNING user_id", values: [username, password, name, email] }, callback);
+    CallPreparedStatement( { name: 'add_user', text: "INSERT INTO codebench.user (username, password, full_name, email) VALUES ($1, crypt($2, gen_salt('bf')), $3, $4) RETURNING user_id", values: [username, password, name, email] }, callback);
 }
 
 var SetQuestionVote = function(userId, questionId, vote, callback) {
@@ -82,17 +82,17 @@ var SetSubmissionVote = function(userId, submissionId, vote, callback) {
 }
 
 var CallPreparedStatement = function(statement, callback) {
-	pg.connect(settings.PgConnString, function(err, client, done) {
-		client.query(statement, function(err, result) {
-			done();
-			if(err) {
-				console.log(err);
-				callback(new Error(err));
-			} else {
-				callback(null, result);
-			}
-		});
-	});
+    pg.connect(settings.PgConnString, function(err, client, done) {
+        client.query(statement, function(err, result) {
+            done();
+            if(err) {
+                console.log(err);
+                callback(new Error(err));
+            } else {
+                callback(null, result);
+            }
+        });
+    });
 }
 
 
--- server.js
+++ server.js
@@ -1,54 +1,54 @@
 var express = require('express'),
-	stylus = require('stylus'),
-	async = require('async'),
-	settings = require('./settings'),
-	queries = require('./queries'),
-	pg = require('pg');
-	//sender = require('./messageSender');
+stylus = require('stylus'),
+async = require('async'),
+settings = require('./settings'),
+queries = require('./queries'),
+pg = require('pg');
+//sender = require('./messageSender');
 var app = express();
 
 app.configure(function() {
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.cookieParser('codebenchyolo'));
-	app.use(express.session('codebenchswag'));
-	app.use(stylus.middleware({
-		src: __dirname + '/views',
-		dest: __dirname + '/public'
-	}));
-	app.set('views', __dirname + '/views');
-	app.use(express.static(__dirname + '/public'));
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser('codebenchyolo'));
+    app.use(express.session('codebenchswag'));
+    app.use(stylus.middleware({
+        src: __dirname + '/views',
+        dest: __dirname + '/public'
+    }));
+    app.set('views', __dirname + '/views');
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(request, response) {
-	console.log(request.cookies.user);
-	response.redirect('/index');
+    console.log(request.cookies.user);
+    response.redirect('/index');
 });
 
 app.get('/question/:id', function(request, response) {
-	queries.GetQuestion(request.params.id, function(err, result) {
-		if(err) {console.log(err);}
-		else {
-			console.log(result);
-		}
-	});
+    queries.GetQuestion(request.params.id, function(err, result) {
+        if(err) {console.log(err);}
+        else {
+            console.log(result);
+        }
+    });
 });
 
 app.post('/logon', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	queries.LoginUser(username, password, function(err, user) {
-		if(err) {
-			console.log(err);
-			response.render('layout.jade', {message: err.message});
-		}
-		else {
-		    expiry = new Date();
-			expiry.setMonth(expiry.getMonth() + 1);
-			response.cookie('user', {'userId' : user.rows[0].user_id, 'username': user.rows[0].username, 'fullName' : user.rows[0].full_name},{expires: expiry, httpOnly:true});
-			response.redirect('/index');
-		}
-	});
+    var username = request.body.username;
+    var password = request.body.password;
+    queries.LoginUser(username, password, function(err, user) {
+        if(err) {
+            console.log(err);
+            response.render('layout.jade', {message: err.message});
+        }
+        else {
+            expiry = new Date();
+            expiry.setMonth(expiry.getMonth() + 1);
+            response.cookie('user', {'userId' : user.rows[0].user_id, 'username': user.rows[0].username, 'fullName' : user.rows[0].full_name},{expires: expiry, httpOnly:true});
+            response.redirect('/index');
+        }
+    });
 });
 
 app.post('/register', function(request, response) {
@@ -58,33 +58,33 @@ app.post('/register', function(request, response) {
     var email    = request.body.email;
 
     queries.AddUser(name, username, password, email, function(err, result) {
-	if(err) {
-	    console.log(err);
-	    response.render('layout.jade', {message: err.message});
-	} else {
-	    expiry = new Date();
-	    expiry.setMonth(expiry.getMonth() + 1);
-	    response.cookie('user', {'userId' : result.user_id, 'username': username, 'fullName' : name},{expires: expiry, httpOnly:true});
-	    response.redirect('/index');
-	}
+        if(err) {
+            console.log(err);
+            response.render('layout.jade', {message: err.message});
+        } else {
+            expiry = new Date();
+            expiry.setMonth(expiry.getMonth() + 1);
+            response.cookie('user', {'userId' : result.user_id, 'username': username, 'fullName' : name},{expires: expiry, httpOnly:true});
+            response.redirect('/index');
+        }
     });
 });
 
 app.get('/problem/:id', function(request, response) {
     var id = request.params.id;
     queries.GetQuestionAndSubmissions(id, function(err, submissions) {
-	if(err) {
-		console.log(err);
-    	} else {
-		console.log(submissions);
-	    queries.GetQuestion(id, function(err, question) {
-		if(err) {
-		    console.log(err);
-		} else {
-		    response.render('post.jade', {user: request.cookies.user, question: question.rows[0], submissions: submissions.rows});
-		}
-	    });
-	}
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(submissions);
+            queries.GetQuestion(id, function(err, question) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    response.render('post.jade', {user: request.cookies.user, question: question.rows[0], submissions: submissions.rows});
+                }
+            });
+        }
     });
 });
 
@@ -94,45 +94,45 @@ app.get('/logoff', function(request, response) {
 });
 
 app.get('/addproblem', function(request, response) {
-	response.render('addQuestion.jade', {user: request.cookies.user});
+    response.render('addQuestion.jade', {user: request.cookies.user});
 });
 
 app.post('/submitQuestion', function(request, response) {
     queries.AddQuestion(request.cookies.user.userId, request.body.problem, request.body.input, request.body.output, function(err, result) {
-    	if(err) {
-    	    response.render('layout.jade', {message: 'Something went wrong', user: request.cookies.user});
-    	} else {
-    	    response.redirect('/problem/'+ result.rows[0].question_id);
-	}
+        if(err) {
+            response.render('layout.jade', {message: 'Something went wrong', user: request.cookies.user});
+        } else {
+            response.redirect('/problem/'+ result.rows[0].question_id);
+        }
     });
 });
 
 app.post('/submitSolution', function(request, response) {
     queries.AddSubmission(request.cookies.user.userId, request.body.problemId, request.body.message, function(err, result) {
-		if(err) {
-    		response.render('layout.jade', {message: 'Something went wrong'});
-		} else {
-			//sender.SendMessage(result.rows[0].submission_id, "java", function(error, msg) {
-			//	console.log(msg);	
-			//});
-    			response.redirect(/submit/ + result.rows[0].submission_id);
-		}
-	});
+        if(err) {
+            response.render('layout.jade', {message: 'Something went wrong'});
+        } else {
+            //sender.SendMessage(result.rows[0].submission_id, "java", function(error, msg) {
+            //  console.log(msg);
+            //});
+            response.redirect(/submit/ + result.rows[0].submission_id);
+        }
+    });
 });
 
 app.post('/setQVote', function(request, response) {
     queries.SetQuestionVote(request.cookies.user.userId, request.body.problemId, request.body.vote, function(err, result) {
         
-    }
-}
+    });
+});
 
 app.get('/submit/:submission_id', function(request, response) {
-	response.render('layout.jade', {message: 'Code submitted! It will be benchmarked soon', user: request.cookies.user});
+    response.render('layout.jade', {message: 'Code submitted! It will be benchmarked soon', user: request.cookies.user});
 });
 
 app.get('/index', function(request, response) {
     queries.GetQuestions( function (err, results) {
-		response.render('index.jade', {user: request.cookies.user, questions: results.rows});
+        response.render('index.jade', {user: request.cookies.user, questions: results.rows});
     });
 });
 
