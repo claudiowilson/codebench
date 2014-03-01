1c39d388cb96fbcff44839be586190862d2eb8a4
Kevin Yeh <kevinyeah@utexas.edu>
2014-02-28 20:16:04 -0500

    Updated index view and things

--- settings.js
+++ settings.js
@@ -3,5 +3,6 @@
 var Rabbit = { url : "amqp://guest:guest@localhost:5672",
 		exchangeName : "codebench" };
 var PgConnString = "postgres://claudiowilson:claudiowilson@localhost:5432/codebench"
+//var PgConnString = "postgres://kyeh:yoloswag@localhost:5432/codebench"
 exports.PgConnString = PgConnString;
 exports.Rabbit = Rabbit;
